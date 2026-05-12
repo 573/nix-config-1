8f463bbcb0bdedd16791ae4c1f9c1f758f25d5b2
Daniel Kahlenberg <573@users.noreply.github.com>
2026-05-12 12:58:30 +0200

    home.base.general: Add osc52 script
    From: https://jvns.ca/til/vim-osc52/

--- home/base/general.nix
+++ home/base/general.nix
@@ -194,11 +194,14 @@ in
             nixfmt-rfc-style
             ;
 
-# see https://jvns.ca/til/vim-osc52/
-/*
-	    pbcopy = #!/bin/bash
-printf "\033]52;c;%s\007" "$(base64 | tr -d '\n')"
-*/
+          # see https://jvns.ca/til/vim-osc52/
+          pbcopy = pkgs.writeShellApplication {
+            name = "pbcopy";
+            runtimeInputs = [ pkgs.coreutils ];
+            text = ''
+              printf "\033]52;c;%s\007" "$(base64 | tr -d '\n')"
+            '';
+          };
 
           #inherit (unstable)
           # eza
